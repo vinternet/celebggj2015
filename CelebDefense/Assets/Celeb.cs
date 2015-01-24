89150fcd0326f1ff1059193eb8bedd8b91a31194
Vin St. John <vinternet2000@optonline.net>
2015-01-23 23:24:32 -0500

    first commit

---
+++ CelebDefense/Assets/Celeb.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Celeb : MonoBehaviour {
+
+	public Vector2 endLocation;
+	public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+		endLocation = new Vector2(7.3, 0);
+
+		walkSpeed = 1f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
