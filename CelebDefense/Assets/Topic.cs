0d7e9741067bd3ed00a3e7b880c32bf89ff21bb8
roguearbiter <harbiter@gmail.com>
2015-01-24 18:45:59 -0500

    adding xml stuff that doesn't do anything for you yet

---
+++ CelebDefense/Assets/Topic.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+public class Topic
+{ 
+	[XmlAttribute("name")]
+	public string Name;
+	
+	[XmlArray("Tweets")]
+	[XmlArrayItem("Tweet")]
+	public List<string> Tweets = new List<string>();
+
+	[XmlArray("Speeches")]
+	[XmlArrayItem("Speech")]
+	public List<string> Speeches = new List<string>();
+
+	public string getRandomTweet(string topic){
+		if (topic != Name)
+						return "nope";
+		return Tweets [0];
+	}
+
+
+}
