aef3fbbea4de59889ae6a28626e4cdc13a11bbc5
Justin Dilks <justin.dilks@gmail.com>
2015-01-24 20:35:35 -0500

    Maybe

---
+++ CelebDefense/Assets/Scripts/CelebBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CelebBehaviour : MonoBehaviour {
+
+	public Transform endGoal;
+	public float walkSpeed = 1f;
+
+	private bool reachedGoal = false;
+	public bool ReachedGoal
+	{
+		get {return reachedGoal;}
+		set {reachedGoal = value;}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (endGoal == null)
+		{
+			endGoal = GameObject.Find("CelebrityEndGoal").transform;
+			if (endGoal == null)
+			{
+				endGoal = this.transform;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (reachedGoal == false)
+		{
+			Vector2 target = new Vector2(endGoal.position.x, endGoal.position.y);
+			Vector2 movementDirection = (target - this.rigidbody2D.position);
+			movementDirection.Normalize();
+			this.rigidbody2D.velocity = walkSpeed * movementDirection;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if(coll.gameObject.name=="CelebrityEndGoal")
+		{
+			reachedGoal = true;
+		}
+	}
+}
---
+++ CelebDefense/Assets/Scripts/PersonWalk.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonWalk : MonoBehaviour {
+
+	public float hopHeight;
+	public float hopTime;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
